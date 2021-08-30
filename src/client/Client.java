1327a7ea2ac71fdaf32edda10056143422395764
saprykin-alex <figulkin@gmail.com>
2021-08-30 05:14:06 +0300

    Работающий чат пока без реализации нормального отключения клиентов Реализация согласно заданию Лабораторной работы №9

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,6 @@
 package client;
 
+import java.awt.event.WindowEvent;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -8,6 +9,7 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
+
     public static void main(String[] args) {
         try {
             Socket socket = new Socket("localhost", 8188);
@@ -48,4 +50,11 @@ public class Client {
             System.out.println("Ошибка!");
         }
     }
+
+    public void Closing(WindowEvent event){
+
+        System.exit(0);
+    }
+
+
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -31,58 +31,45 @@ public class Server {
                         @Override
                         public void run() {
                             try {
-//                                System.out.println("Передаю сообщение клиенту №"+currentUser.getUserNumber());
-//                                currentUser.getOos().writeObject("Добро пожаловать на сервер");
-//                                currentUser.getOos().writeObject("Введите ваше имя: ");
                                 out.writeUTF("Добро пожаловать на сервер!");
-                                out.writeUTF("Введите ваше имя: ");
-                                String userName = in.readUTF(); // Ожидаем имя от клиента
+                                //Ждём от клиента ввода имени и проверяем его на уникальность
+                                String userName;
+                                do {
+                                    out.writeUTF("Введите ваше имя: ");
+                                    userName = in.readUTF(); // Ожидаем имя от клиента
+                                }while (existUserName(users, userName, out));//Если true, то заново просим ввести имя
                                 currentUser.setUserName(userName);
                                 usersName.add(currentUser.getUserName());
-                                String onlineUsers="К чату подключены: Вы";
-                                for (User user:users) {
-                                    if (user.getUserName() != null) {
-                                        DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
-                                        if (!user.equals(currentUser)) {
-                                            onlineUsers += ", " + user.getUserName();
-                                            out.writeUTF(currentUser.getUserName() + " присоединился к беседе");
-                                        } else {
-                                            out.writeUTF(onlineUsers);
-                                        }
-                                    }
-                                }
-//                                for (User user : users) {
-//                                    DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
-//                                    out.writeUTF(currentUser.getUserName()+" присоединился к беседе");
-//                                    out.writeUTF(onlineUsers);
-//                                }
+                                //Рассылка сообщений всем клиентам о подключении нового пользователя
+                                searchForUsers(users, currentUser, "К чату подключены только Вы", "К чату подключены: Вы", " присоединился к беседе");
                                 while (true){
                                     String request = in.readUTF(); // Ждём сообщение от пользователя
-                                    //Сюда код по распарсиванию имени
-
-                                    System.out.println(currentUser.getUserName()+": "+request);
-                                    for (User user : users) {
-                                        if(users.indexOf(user) == users.indexOf(currentUser)) continue;
-                                        DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
-                                        out.writeUTF(currentUser.getUserName()+": "+request);
-                                    }
+                                    //Распарсивание сообщения
+                                    String[] splited = request.split(" ");
+                                        if (splited[0].equals("/m") && splited.length>2) {
+                                            User user = existUser(splited[1], users);
+                                            if (user!=null){
+                                                DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
+                                                String str = "";
+                                                for (int i = 2; i < splited.length; i++) {
+                                                    str+=splited[i];
+                                                }
+                                                out.writeUTF(currentUser.getUserName()+": "+str);
+                                            }
+                                        }else {
+                                            System.out.println(currentUser.getUserName()+": "+request);
+                                            for (User user : users) {
+                                                if(users.indexOf(user) == users.indexOf(currentUser)) continue;
+                                                DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
+                                                out.writeUTF(currentUser.getUserName()+": "+request);
+                                            }
+                                        }
                                 }
                             }catch (IOException e){
+                                //Рассылка всем клиентам об отключении пользователя
+                                searchForUsers(users, currentUser, "Вы покинули чат", "Вы покинули чат, но в нём остались: ", " покинул чат");
                                 users.remove(currentUser);
-                                String onlineUsers="onlineUsers";
-                                for (User user:users) {
-                                    onlineUsers+="//"+user.getUserName();
-                                }
-
-                                for (User user : users) {
-                                    try {
-                                        DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
-                                        out.writeUTF(currentUser.getUserName()+" покинул чат");
-                                        out.writeUTF(onlineUsers);
-                                    } catch (IOException ioException) {
-                                        ioException.printStackTrace();
-                                    }
-                                }
+                                usersName.remove(currentUser.getUserName());
                             }
                         }
                     });
@@ -93,5 +80,59 @@ public class Server {
             }
         }
     }
+    //Метод для рассылки о входе и выходе из чата
+    private static void searchForUsers(ArrayList<User> users, User currentUser, String onlineUsers1, String onlineUsers2, String str) {
+        int i = 0;
+        String onlineUsers = "";
+            for (User user : users) {         //Перебор всех клиентов
+                if (user.getUserName() != null) {       //Если клиент подключился, но не ввёл своё имя, то не отправляем ему сообщение
+                    try {
+                        DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
+                        if (!user.equals(currentUser)) {
+                            out.writeUTF(currentUser.getUserName() + str);         //Отправляем сообщение всем клиентам, кроме текущего
+                            onlineUsers += ", " + user.getUserName();   //Список клиентов в строке
+                            i++;
+                        }
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        try {
+            DataOutputStream out = new DataOutputStream(currentUser.getSocket().getOutputStream());     //Текущему пользователю сообщаем кто ещё есть в чате
+                if (i > 0) {
+                    onlineUsers=onlineUsers2+onlineUsers;
+                } else {
+                    onlineUsers = onlineUsers1;
+                }
+            out.writeUTF(onlineUsers);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    //Функция проверки уникальности имени подключающегося клиента
+    private static boolean existUserName(ArrayList<User> users, String userName, DataOutputStream out){
+        User user = existUser(userName, users);
+        if(user!=null){
+            try {
+                out.writeUTF("Такой никнейм уже присутствует в чате!");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    private static User existUser(String userName, ArrayList<User> users){
+        for (User user:users) {
+            if(user.getUserName()!=null) {
+                if (user.getUserName().equals(userName)) return user;
+            }
+        }
+        return null;
+    }
+
 }
 
